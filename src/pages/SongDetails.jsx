02817b87b7755387b3398ba5c80614081f4316d9
Gigiox <guillermo.casanova.b@gmail.com>
2022-09-28 01:22:53 -0300

    🐛 Fixing bugs

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -11,7 +11,7 @@ import {
 
 const SongDetails = () => {
   const dispatch = useDispatch();
-  const { songId } = useParams();
+  const { songId, id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songId });
@@ -39,13 +39,19 @@ const SongDetails = () => {
 
   return (
     <div className='flex flex-col'>
-      <DetailsHeader artistId='' songData={songData} />
+      <DetailsHeader artistId={artistId} songData={songData} />
+
       <div className='mb-10'>
         <h2 className='text-white text-3xl font-bold'>Lyrics:</h2>
         <div className='mt-5'>
           {songData?.sections[1].type === 'LYRICS' ? (
-            songData?.sections[1].text.map((line, i) => (
-              <p className='text-gray-400 text-base my-1'>{line}</p>
+            songData?.sections[1]?.text.map((line, i) => (
+              <p
+                key={`lyrics-${line}-${i}`}
+                className='text-gray-400 text-base my-1'
+              >
+                {line}
+              </p>
             ))
           ) : (
             <p className='text-gray-400 text-base my-1'>
@@ -54,8 +60,10 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+
       <RelatedSongs
         data={data}
+        artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
         handlePauseClick={handlePauseClick}
