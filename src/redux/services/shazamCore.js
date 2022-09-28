4ea6e9265e6c9a1adae31346e6b6145642aa7bda
Gigiox <guillermo.casanova.b@gmail.com>
2022-09-28 02:14:29 -0300

    ✨ Added endpoint for searchByTerm

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -38,6 +38,10 @@ export const shazamCoreApi = createApi({
     getSongsByCountry: builder.query({
       query: (countryCode) => `/charts/country?country_code=${countryCode}`,
     }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
   }),
 });
 
@@ -48,4 +52,5 @@ export const {
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
 } = shazamCoreApi;
